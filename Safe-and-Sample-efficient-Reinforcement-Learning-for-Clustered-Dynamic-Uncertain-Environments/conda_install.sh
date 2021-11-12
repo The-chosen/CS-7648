222e7077e5c0e7a20c8c2de8c6688ecbb919ae8c
The-chosen <the-chosen-yy@outlook.com>
2021-11-12 18:38:20 -0500

    Add Scripts

--- Safe-and-Sample-efficient-Reinforcement-Learning-for-Clustered-Dynamic-Uncertain-Environments/conda_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ~/tmp
-curl -O https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh
-sha256sum Anaconda3-2021.05-Linux-x86_64.sh
-bash Anaconda3-2021.05-Linux-x86_64.sh
