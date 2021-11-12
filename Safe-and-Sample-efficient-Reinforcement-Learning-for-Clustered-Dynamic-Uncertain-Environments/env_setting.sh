ec25b74393d93ed5890fc387984002de421967e6
The-chosen <the-chosen-yy@outlook.com>
2021-11-12 17:54:57 -0500

    Add Scripts

--- Safe-and-Sample-efficient-Reinforcement-Learning-for-Clustered-Dynamic-Uncertain-Environments/env_setting.sh
+++ Safe-and-Sample-efficient-Reinforcement-Learning-for-Clustered-Dynamic-Uncertain-Environments/env_setting.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/The-chosen/CS-7648.git
+# git clone https://github.com/The-chosen/CS-7648.git
 cd /tmp
 curl -O https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh
 sha256sum Anaconda3-2021.05-Linux-x86_64.sh
