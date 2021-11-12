e6265e31ffb43ec9173625fe57ee09b50b08d5d4
The-chosen <the-chosen-yy@outlook.com>
2021-11-12 18:56:31 -0500

    Add Scripts

--- Safe-and-Sample-efficient-Reinforcement-Learning-for-Clustered-Dynamic-Uncertain-Environments/env_setting.sh
+++ Safe-and-Sample-efficient-Reinforcement-Learning-for-Clustered-Dynamic-Uncertain-Environments/env_setting.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-cd ~/tmp
-curl -O https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh
-sha256sum Anaconda3-2021.05-Linux-x86_64.sh
-bash Anaconda3-2021.05-Linux-x86_64.sh
-source ~/.bashrc
 cd ~/CS-7648/Safe-and-Sample-efficient-Reinforcement-Learning-for-Clustered-Dynamic-Uncertain-Environments/
 conda create -n safe-rl python=3.7.9 -y
 conda activate safe-rl
