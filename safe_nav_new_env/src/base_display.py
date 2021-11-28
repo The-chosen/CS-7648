05f85a9a6b62d381675fa4ad803a85a43d4c442e
schen754.800 <schen754@gatech.edu>
2021-11-28 12:19:36 -0500

    clean up base_display

--- safe_nav_new_env/src/base_display.py
+++ safe_nav_new_env/src/base_display.py
@@ -1,54 +1,27 @@
 class BaseDisplay():
 
     def setup(self, x_bounds, y_bounds,
-              in_bounds, goal_bounds,
-              margin, static_obs):
+                  in_bounds, goal_bounds,
+                  margin, static_obstacles):
         pass
-    
+       
     def begin_time_step(self, t):
         pass
 
-    def obstacle_at_loc(self, i, x, y):
+    def end_time_step(self, t):
         pass
 
-    def obstacle_estimated_at_loc(self, i, x, y, is_match=False):
+    def obstacle_at_loc(self, i, x, y, nearest_obstacle = False, close_obstacle = False):
         pass
 
-    def obstacle_estimates_compared(self, num_matched, num_total):
+    def obstacle_set_color(self, i, color = 'grey' ):
         pass
 
-    def obstacle_set_color(self, id, color):
-        pass
-
-    def robot_at_loc(self, x, y, h, is_safe):
-        pass
-
-    def robot_steers(self, dh, dv):
-        pass
-
-    def collision(self):
-        pass
-
-    def out_of_bounds(self):
-        pass
-
-    def goal(self):
+    def robot_at_loc(self, x, y, h, is_ssa = False):
         pass
 
     def navigation_done(self, retcode, t):
         pass
 
-    def estimation_done(self, retcode, t):
-        pass
-    
-    def end_time_step(self, t):
-        pass
-
-    def teardown(self):
-        pass
-
     def render(self):
-        pass
-
-    def _reset(self):
         pass
